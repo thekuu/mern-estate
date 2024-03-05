bbd9bf74ee9381e4f7c2c9ae75f59e16cf375a85
thekuu <zekaryasgeremew@gmail.com>
2024-03-05 20:35:50 +0300

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
         <input type = 'text' placeholder = 'username' className='border p-3 rounded-lg' id='username' onChange = {handleChange} required/>
         <input type = 'email' placeholder = 'email' className='border p-3 rounded-lg' id='email' onChange = {handleChange} required/>
         <input type = 'password' placeholder = 'password' className='border p-3 rounded-lg' id='password' onChange = {handleChange} required/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...' : 'Sign up'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...' : 'Sign Up'}</button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
