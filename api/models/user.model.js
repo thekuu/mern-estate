8ad8179d48534873b3887f17f8c2b64d42081038
thekuu <zekaryasgeremew@gmail.com>
2024-04-26 22:18:15 +0300

    complete update user functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        defaul: "https://www.bing.com/th?id=OIP.UahRrl99Crt1ybciDDN-pAHaHx&w=150&h=157&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2://www.bing.com/images/search?q=profile+icon+for+website&id=94002B2B27577511EBF1BA5D788B9114358003A6&FORM=IQFRBA",
+        default: "https://www.bing.com/th?id=OIP.UahRrl99Crt1ybciDDN-pAHaHx&w=150&h=157&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2://www.bing.com/images/search?q=profile+icon+for+website&id=94002B2B27577511EBF1BA5D788B9114358003A6&FORM=IQFRBA",
     }
 }, {timestamps: true});
 
