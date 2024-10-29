df6ffb76c87a8d6ab212ecb040a4bb7665b65b63
thekuu <127185902+thekuu@users.noreply.github.com>
2024-10-30 00:35:50 +0300

    add shoew more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,7 +15,7 @@ function Search() {
     })
     const [loading, setLoading] = useState(false)
     const [listings, setListings] = useState([])
-    console.log(listings)
+    const [showMore, setShowMore] = useState(false)
     
     useEffect(() => {
       const urlParams  = new URLSearchParams(location.search);
@@ -47,9 +47,15 @@ function Search() {
       }
       const fetchListings = async () => {
         setLoading(true)
+        setShowMore(false)
         const searchQuery = urlParams.toString()
         const res = await fetch(`/api/listing/get?${searchQuery}`)
         const data = await res.json();
+        if(data.length > 9) {
+          setShowMore(true);
+        } else {
+          setShowMore(false)
+        }
         setListings(data);
         setLoading(false)
       }
@@ -85,7 +91,19 @@ function Search() {
       const searchQuery = urlParams.toString()
       navigate(`/search?${searchQuery}`)
     }
-
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length
+    const startIndex = numberOfListings
+    const urlParams = new URLSearchParams(location.search)
+    urlParams.set('startIndex', startIndex)
+    const searchQuery = urlParams.toString()
+    const res = await fetch(`/api/listing/get?${searchQuery}`)
+    const data = await res.json()
+    if (data.length < 9) {
+      setShowMore([...listings, ...data])
+    }
+    setListings(data)
+  }
   return (
     <div className="flex flex-clo md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md: min-h-screen">
@@ -155,6 +173,9 @@ function Search() {
           listings.map((listing) => (
             <ListingItem key={listing._id} listing={listing}/>
           ))
+        )}
+        {showMore && (
+          <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>Show More</button>
         )}
         </div>
       </div>
